6a61111325bd597f16b57746a758b09cba2bf161
LucasColvero <144968002+AlunoLucasGC@users.noreply.github.com>
2026-09-20 22:54:53 -0300

    chore: inicio do projeto Colvero

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+#Importando Flask e renderizando arquivos HTML
+from flask import Flask, render_template
+
+#Criando a Aplicação
+app= Flask(__name__)
+
+#Criação da rota que representa a página inicial
+@app.route("/")
+@app.route("/sobre")
+
+def inicio():
+    return render_template("index.html")
+
+def sobre ():
+    return render_template("sobre.html")
+    
+#Inicia o servidor
+if __name__ == "__main__": 
+    app.run(debug = True) 
